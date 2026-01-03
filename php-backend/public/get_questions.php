092228171b0e83225e9d1bfb879a31782fa0f859
Ghassani <ghassanihh@gmail.com>
2026-01-04 00:13:29 +0200

    fixx

--- php-backend/public/get_questions.php
+++ php-backend/public/get_questions.php
@@ -15,3 +15,20 @@ while ($row = $result->fetch_assoc()) {
 }
 
 echo json_encode($questions);
+<?php
+include "index.php";
+
+$sql = "SELECT * FROM questions";
+$result = $mysqli->query($sql);
+
+$questions = [];
+
+while ($row = $result->fetch_assoc()) {
+    $questions[] = [
+        "id" => (string)$row["id"],
+        "title" => $row["title"],
+        "options" => json_decode($row["options"], true)
+    ];
+}
+
+echo json_encode($questions);
